c53d1bf19f8148d8f1d6b11ee6014086d0ee29e5
Stephen Roebuck <controll.face@gmail.com>
2024-02-16 16:03:07 -0500

    turning off points and bounds renderers

--- src/main/java/com/controllerface/bvge/game/TestGame.java
+++ src/main/java/com/controllerface/bvge/game/TestGame.java
@@ -36,9 +36,7 @@ public class TestGame extends GameMode
     private static final EnumSet<RenderType> ACTIVE_RENDERERS =
         EnumSet.of(
             RenderType.MODELS,
-            RenderType.HULLS,
-            RenderType.BOUNDS,
-            RenderType.POINTS);
+            RenderType.HULLS);
 
 
     public TestGame(ECS ecs, GameSystem screenBlankSystem)
@@ -242,7 +240,7 @@ public class TestGame extends GameMode
             ecs.registerSystem(new PointRenderer(ecs));
         }
 
-        ecs.registerSystem(new BoneRenderer(ecs));
+        //ecs.registerSystem(new BoneRenderer(ecs));
     }
 
     @Override
