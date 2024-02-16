be1dee2f86ff95e0da4f91d8d89a17940dce956b
Stephen Roebuck <controll.face@gmail.com>
2024-02-16 15:16:13 -0500

    removed test code

--- src/main/java/com/controllerface/bvge/Main.java
+++ src/main/java/com/controllerface/bvge/Main.java
@@ -11,10 +11,6 @@ public class Main
 
     public static void main(String[] args)
     {
-        String bits = System.getProperty("sun.arch.data.model");
-
-        System.out.println("debug: bits=" + bits);
-
         //Configuration.DISABLE_CHECKS.set(true);
         Window window = Window.get();
         window.init();
