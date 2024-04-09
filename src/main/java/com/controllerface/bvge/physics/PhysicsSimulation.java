2df9591a70249310e9fab74858d14ee3ffadcef8
controllerface <controllerface@hotmail.com>
2024-04-08 20:53:50 -0400

    clean up spacing

--- src/main/java/com/controllerface/bvge/physics/PhysicsSimulation.java
+++ src/main/java/com/controllerface/bvge/physics/PhysicsSimulation.java
@@ -154,19 +154,19 @@ public class PhysicsSimulation extends GameSystem
         counts_data_ptr     = GPGPU.cl_new_buffer(counts_buf_size);
         offsets_data_ptr    = GPGPU.cl_new_buffer(counts_buf_size);
 
-        point_reaction_counts   = new TransientBuffer(CLSize.cl_int, 500_000L);
-        point_reaction_offsets  = new TransientBuffer(CLSize.cl_int, 500_000L);
-        reactions_in            = new TransientBuffer(CLSize.cl_float8, 500_000L);
-        reactions_out           = new TransientBuffer(CLSize.cl_float8, 500_000L);
-        reaction_index          = new TransientBuffer(CLSize.cl_int, 500_000L);
-        key_map                 = new TransientBuffer(CLSize.cl_int, 500_000L);
-        key_bank                = new TransientBuffer(CLSize.cl_int, 500_000L);
-        in_bounds               = new TransientBuffer(CLSize.cl_int, 500_000L);
-        candidates              = new TransientBuffer(CLSize.cl_int2, 500_000L);
-        candidate_counts        = new TransientBuffer(CLSize.cl_int2, 500_000L);
-        candidate_offsets       = new TransientBuffer(CLSize.cl_int, 500_000L);
-        matches                 = new TransientBuffer(CLSize.cl_int, 500_000L);
-        matches_used            = new TransientBuffer(CLSize.cl_int, 500_000L);
+        point_reaction_counts       = new TransientBuffer(CLSize.cl_int, 500_000L);
+        point_reaction_offsets      = new TransientBuffer(CLSize.cl_int, 500_000L);
+        reactions_in                = new TransientBuffer(CLSize.cl_float8, 500_000L);
+        reactions_out               = new TransientBuffer(CLSize.cl_float8, 500_000L);
+        reaction_index              = new TransientBuffer(CLSize.cl_int, 500_000L);
+        key_map                     = new TransientBuffer(CLSize.cl_int, 500_000L);
+        key_bank                    = new TransientBuffer(CLSize.cl_int, 500_000L);
+        in_bounds                   = new TransientBuffer(CLSize.cl_int, 500_000L);
+        candidates                  = new TransientBuffer(CLSize.cl_int2, 500_000L);
+        candidate_counts            = new TransientBuffer(CLSize.cl_int2, 500_000L);
+        candidate_offsets           = new TransientBuffer(CLSize.cl_int, 500_000L);
+        matches                     = new TransientBuffer(CLSize.cl_int, 500_000L);
+        matches_used                = new TransientBuffer(CLSize.cl_int, 500_000L);
 
         control_point_flags         = new PersistentBuffer(CLSize.cl_int, 1);
         control_point_indices       = new PersistentBuffer(CLSize.cl_int, 1);
