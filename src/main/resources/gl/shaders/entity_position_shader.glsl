828f2337830a927e4b00f12b541720c3eeeb2b9e
Stephen Roebuck <controll.face@gmail.com>
2024-06-13 20:35:48 -0400

    pushing renamed shader

---
+++ src/main/resources/gl/shaders/entity_position_shader.glsl
@@ -0,0 +1,20 @@
+#type vertex
+#version 450 core
+layout (location = 0) in vec2 v_position;
+
+uniform mat4 uVP;
+
+void main()
+{
+    gl_Position =  uVP * vec4(v_position, 5.0, 1.0);
+}
+
+#type fragment
+#version 450 core
+
+out vec4 color;
+
+void main()
+{
+    color = vec4(0.0, 1.0, 0.0, 1.0);
+}
