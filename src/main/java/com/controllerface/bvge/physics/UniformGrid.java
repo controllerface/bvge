3176be78cf0d417e21837e74c9b9838d1de876ec
Stephen Roebuck <controll.face@gmail.com>
2024-06-22 00:05:18 -0400

    spacing change

--- src/main/java/com/controllerface/bvge/physics/UniformGrid.java
+++ src/main/java/com/controllerface/bvge/physics/UniformGrid.java
@@ -89,7 +89,7 @@ public class UniformGrid
         this.sector_origin_x = sector_origin_x;
         this.sector_origin_y = sector_origin_y;
         this.sector_width    = sector_width;
-        this.sector_height    = sector_height;
+        this.sector_height   = sector_height;
     }
 
     public void updateOrigin(float x_origin, float y_origin, float x_player, float y_player)
