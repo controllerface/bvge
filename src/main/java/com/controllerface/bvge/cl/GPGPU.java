ddceea4250ac2ad432c8b87841412be52519c341
Stephen Roebuck <controll.face@gmail.com>
2024-07-10 15:13:23 -0400

    testing unpinned int usage

--- src/main/java/com/controllerface/bvge/cl/GPGPU.java
+++ src/main/java/com/controllerface/bvge/cl/GPGPU.java
@@ -387,8 +387,9 @@ public class GPGPU
 
     public static long cl_new_pinned_int()
     {
-        long flags = CL_MEM_HOST_READ_ONLY | CL_MEM_ALLOC_HOST_PTR;
-        return clCreateBuffer(ptr_context, flags, CLSize.cl_int, null);
+        return cl_new_unpinned_int();
+//        long flags = CL_MEM_HOST_READ_ONLY | CL_MEM_ALLOC_HOST_PTR;
+//        return clCreateBuffer(ptr_context, flags, CLSize.cl_int, null);
     }
 
     public static long cl_new_unpinned_int()
@@ -434,22 +435,23 @@ public class GPGPU
 
     public static int cl_read_pinned_int(long queue_ptr, long pinned_ptr)
     {
-        var out = clEnqueueMapBuffer(queue_ptr,
-            pinned_ptr,
-            true,
-            CL_MAP_READ,
-            0,
-            CLSize.cl_int,
-            null,
-            null,
-            (IntBuffer) null,
-            null);
-
-        assert out != null;
-
-        int result = out.order(ByteOrder.LITTLE_ENDIAN).asIntBuffer().get(0);
-        clEnqueueUnmapMemObject(queue_ptr, pinned_ptr, out, null, null);
-        return result;
+        return cl_read_unpinned_int(queue_ptr, pinned_ptr);
+//        var out = clEnqueueMapBuffer(queue_ptr,
+//            pinned_ptr,
+//            true,
+//            CL_MAP_READ,
+//            0,
+//            CLSize.cl_int,
+//            null,
+//            null,
+//            (IntBuffer) null,
+//            null);
+//
+//        assert out != null;
+//
+//        int result = out.order(ByteOrder.LITTLE_ENDIAN).asIntBuffer().get(0);
+//        clEnqueueUnmapMemObject(queue_ptr, pinned_ptr, out, null, null);
+//        return result;
     }
 
     public static void cl_transfer_buffer(long queue_ptr, long src_ptr, long dst_ptr, long size)
