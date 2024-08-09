1e213998637891dd8e7b79624231aff40899ee2c
Stephen Roebuck <controll.face@gmail.com>
2024-08-08 20:38:57 -0400

    condensed GPGPU down to bare minimum

--- src/main/java/com/controllerface/bvge/gpu/cl/GPGPU.java
+++ src/main/java/com/controllerface/bvge/gpu/cl/GPGPU.java
@@ -3,42 +3,11 @@ package com.controllerface.bvge.gpu.cl;
 import com.controllerface.bvge.ecs.ECS;
 import com.controllerface.bvge.gpu.GPU;
 import com.controllerface.bvge.memory.GPUCoreMemory;
-import org.lwjgl.BufferUtils;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
-/**
- * Core class used for executing General Purpose GPU (GPGPU) functions
- */
 public class GPGPU
 {
-    //#region Constants
-
-    /**
-     * A convenience object, used when clearing out buffers to fill them with zeroes
-     */
-    private static final ByteBuffer ZERO_PATTERN_BUFFER = BufferUtils.createByteBuffer(4).order(ByteOrder.nativeOrder());
-    private static final ByteBuffer NEGATIVE_ONE_PATTERN_BUFFER = BufferUtils.createByteBuffer(4).order(ByteOrder.nativeOrder());
-
-    static
-    {
-        ZERO_PATTERN_BUFFER.putInt(0,0);
-        NEGATIVE_ONE_PATTERN_BUFFER.putInt(0, -1);
-    }
-
-    //#endregion
-
-    //#region Workgroup Variables
-
     public static GPUCoreMemory core_memory;
     public static CL_ComputeController compute;
-
-    //#endregion
-
-
-    //#region Misc. Public API
-
 
     public static void init(ECS ecs)
     {
@@ -51,6 +20,4 @@ public class GPGPU
         core_memory.release();
         compute.release();
     }
-
-    //#endregion
 }
